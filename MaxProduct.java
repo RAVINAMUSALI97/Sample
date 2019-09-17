7ae78b166e9d00490e2a39a6a4ce286241358ca2
RAVINAMUSALI97 <38503388+RAVINAMUSALI97@users.noreply.github.com>
2019-09-17 16:30:26 +0530

    Update MaxProduct.java

--- MaxProduct.java
+++ MaxProduct.java
@@ -1,20 +1,35 @@
 class MaxProduct {
   public static void main(String[] args) {
-    
-        Scanner sc=new Scanner(System.in);
+    Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int[] arr=new int[n];
         for(int i=0;i<n;i++) {
             arr[i]=sc.nextInt();
         }
-        int sum=0,sum1=0;
+        int sum=0,sum1=0,s1=0,s2=0;
         int j=0,k=1;
         for(j=0;j<arr.length;j++) {
             sum+=arr[j]*(j+1);
             if(arr[j]>=0) {
                 sum1+=arr[j]*k++;
             }
+            else {
+                s1=0;
+                s2=0;
+                for(int i=0;i<j;i++) {
+                    s1+=arr[i];
+                }
+                for(int i=j+1;i<n;i++) {
+                    s2+=arr[i];
+                }
+                if(s1<=s2) {
+                    sum1+=arr[j]*k++;
+                }
+                //System.out.println(s1+" "+s2);
+            }
+            
         }
+        
         int a=(sum1>sum)?sum1:sum;
         System.out.println("-----"+a+"-----");
-}
+  }
